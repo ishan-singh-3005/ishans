9cc1444f17935f672101e664c4cb0bb0e5c879ba
Ishan Singh <ishan423@gmail.com>
2021-03-03 09:48:45 -0500

    updated info

---
+++ src/Components/Contact.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+            <a href="mailto:ishan423@gmail.com"><h1><span>Get In Touch.</span></h1></a>
+
+            </div>
+
+            <div className="ten columns">
+
+                  <p className="lead">{message}</p>
+
+            </div>
+
+         </div>
+
+         <div className="widget widget_contact">
+
+            <h4>Address and Phone</h4>
+            <p className="address">
+               {name}<br />
+               {street} <br />
+               {city}, {state} {zip}<br />
+               <span>{phone}</span><br />
+               <a href="mailto:ishan423@gmail.com"><span>{email}</span></a>
+            </p>
+         </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
